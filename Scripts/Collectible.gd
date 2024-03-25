668f5efa575abb2cfd21b538aea7c8209739d291
mehmetxav <mehmetav1235@gmail.com>
2024-03-25 13:10:57 -0400

    changes

--- Scripts/Collectible.gd
+++ Scripts/Collectible.gd
@@ -7,8 +7,6 @@ func _on_body_entered(body):
 	# when player enters
 	# increment apple count by 1
 	global.apple_count = global.apple_count + 1
-	print("apple count", global.apple_count)
-	print(body)
 	body.play_pickup_sound()
 	emit_signal("item_collected")
 	queue_free() # remove object from scene((
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_moneycase_item_collected():
-	$CenterContainer/briefcasecount.text = global.apple_count
+	$CenterContainer/briefcasecount.text = str(global.apple_count) 
