3123a3de9d90ff519009feca7e7bf81eaf7e90e0
mehmetxav <mehmetav1235@gmail.com>
2024-03-25 12:04:36 -0400

    added start and quit function

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -3,7 +3,11 @@ extends Control
 
 @export var start_level_scene : PackedScene
 
+
+
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(start_level_scene)
 
 
+func _on_quit_pressed():
+		get_tree().quit()
