2b85d3539ac4438c5a3517d146520aa687b6c586
mehmetxav <143541655+mehmetxav@users.noreply.github.com>
2024-03-26 14:47:26 -0400

    Bug fixes.Finished up the hud for the ingame picking up the moneycases.

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,4 +1,12 @@
 extends Control
 
+func _ready():
+	$CenterContainer2/Console.text = ""
+
 func _on_moneycase_item_collected():
 	$CenterContainer/briefcasecount.text = str(global.apple_count) 
+
+
+func _on_pickuparea_update_console(message):
+	$CenterContainer2/Console.text = message
+
