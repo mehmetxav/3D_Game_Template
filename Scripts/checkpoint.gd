6197f27ef3966fed451b9a3082c85d59a16faeaf
mehmetxav <143541655+mehmetxav@users.noreply.github.com>
2024-04-16 15:53:25 -0400

    new assets plus checkpoint

---
+++ Scripts/checkpoint.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+@export var player_respawn_position : Node3D
+
+var is_activated : bool = false
+
+
+
+func _on_body_entered(_body):
+	if not is_activated:
+		is_activated = true
+		
+		# change player respawn point
+		player_respawn_position.position = position
+		
+		$AnimationPlayer.play("activated")
+		await $AnimationPlayer.animation_finished
+		$AnimationPlayer.play("idle")
+	
