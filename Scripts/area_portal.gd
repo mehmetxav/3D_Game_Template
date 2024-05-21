9a2a572b16235c9a482c513a243534a8e124f1c4
mehmetxav <143541655+mehmetxav@users.noreply.github.com>
2024-05-21 16:08:30 -0400

    a lot of changes

--- Scripts/area_portal.gd
+++ Scripts/area_portal.gd
@@ -6,15 +6,13 @@ var player_entered = false
 signal update_console
 
 func _unhandled_input(_event):
-	if Input.is_action_just_pressed("portal") and global.has_special_key and global.apple_count >= 3:
+	if Input.is_action_just_pressed("portal") and global.has_special_key:
 			get_tree().change_scene_to_file(level_to_load)
 
 func _on_body_entered(_body):
 	player_entered = true
 	if not global.has_special_key:
 		emit_signal("update_console","You need to find the special key")
-	elif global.apple_count < 3:
-		emit_signal("update_console", "You need 3 cases to go to the next level")
 	else:
 		emit_signal("update_console", "Press G to go to the next level")
 
